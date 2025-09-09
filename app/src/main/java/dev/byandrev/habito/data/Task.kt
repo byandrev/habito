0bebbde25213ae66b26099e1b476df275ae6e65b
Andres Parra <byandrev@gmail.com>
2025-09-09 13:38:12 -0500

    feat: add Task entity and DAO

---
+++ app/src/main/java/dev/byandrev/habito/data/Task.kt
@@ -0,0 +1,11 @@
+package dev.byandrev.habito.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val checked: Boolean
+)
---
+++ app/src/main/java/dev/byandrev/habito/data/TaskDao.kt
@@ -0,0 +1,22 @@
+package dev.byandrev.habito.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Insert
+    suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+
+    @Query("SELECT * FROM tasks")
+    fun getAllTasks(): Flow<List<Task>>
+
+    @Query("SELECT * FROM tasks WHERE id = :id")
+    suspend fun getTaskById(id: Int): Task?
+}
