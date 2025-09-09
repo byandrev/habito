3cc35ad2ea3a86781533afee47421329d8e98265
Andres Parra <byandrev@gmail.com>
2025-09-09 13:44:37 -0500

    feat: implement basic navigation with Compose Navigation

--- app/src/main/java/dev/byandrev/habito/ui/components/NavigationBar.kt
+++ app/src/main/java/dev/byandrev/habito/ui/components/NavigationBar.kt
@@ -1,12 +1,7 @@
 package dev.byandrev.habito.ui.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
@@ -15,7 +10,6 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,8 +19,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -43,6 +35,7 @@ enum class Destination(
     TASKS("tasks", "Tasks", Icons.Default.List, "Tasks"),
 }
 
+@SuppressLint("ViewModelConstructorInComposable")
 @Preview()
 @Composable
 fun NavigationBar(modifier: Modifier = Modifier) {
