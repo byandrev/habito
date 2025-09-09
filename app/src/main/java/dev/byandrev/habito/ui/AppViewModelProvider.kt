f0647eea8b5685efc5174f00240949a19285a999
Andres Parra <byandrev@gmail.com>
2025-09-09 13:43:55 -0500

    feat: add ViewModel factory for TaskViewModel

---
+++ app/src/main/java/dev/byandrev/habito/ui/AppViewModelProvider.kt
@@ -0,0 +1,19 @@
+package dev.byandrev.habito.ui
+
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import dev.byandrev.habito.HabitoApplication
+import dev.byandrev.habito.viewmodel.TaskViewModel
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            TaskViewModel(habitoApplication().container.tasksRepository)
+        }
+    }
+}
+
+fun CreationExtras.habitoApplication(): HabitoApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as HabitoApplication)
