692c4a6cfbe5ca447009722949286de2613614c2
Andres Parra <byandrev@gmail.com>
2025-10-06 19:37:21 -0500

    refactor: remove unnecessary imports in Login and Theme files

--- app/src/main/java/dev/byandrev/habito/ui/screens/Login.kt
+++ app/src/main/java/dev/byandrev/habito/ui/screens/Login.kt
@@ -6,10 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,13 +15,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.byandrev.habito.ui.components.CustomButton
 import dev.byandrev.habito.ui.components.Logo
-import dev.byandrev.habito.ui.theme.HabitoTheme
 import dev.byandrev.habito.viewmodel.AuthViewModel
 
 @Composable
