f95542f1121d1a36c8e5f2e25125bd12c95f601e
Andres Parra <byandrev@gmail.com>
2025-10-14 11:02:49 -0500

    feat: add navigation and action icons to header

--- app/src/main/java/dev/byandrev/habito/ui/components/Header.kt
+++ app/src/main/java/dev/byandrev/habito/ui/components/Header.kt
@@ -1,10 +1,13 @@
 package dev.byandrev.habito.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -20,8 +23,23 @@ fun Header() {
                 fontWeight = FontWeight.Bold,
             )
         },
-//        colors = TopAppBarDefaults.topAppBarColors(
-//            containerColor = MaterialTheme.colorScheme.surfaceContainer
-//        )
+
+        navigationIcon = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+
+        actions = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
     )
 }
