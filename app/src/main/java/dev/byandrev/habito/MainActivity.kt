c5fba28d1b8cea3b122678796e3f44799886dc4e
Andres Parra <byandrev@gmail.com>
2025-10-14 10:57:17 -0500

    refactor: remove unused imports and update .gitignore
    This commit removes unused imports from `MainActivity.kt` and adds the `.idea` directory to the `.gitignore` file.

--- app/src/main/java/dev/byandrev/habito/MainActivity.kt
+++ app/src/main/java/dev/byandrev/habito/MainActivity.kt
@@ -1,17 +1,14 @@
 package dev.byandrev.habito
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.firebase.auth.FirebaseAuth
 import dev.byandrev.habito.ui.components.NavigationBar
 import dev.byandrev.habito.ui.screens.LoadingScreen
 import dev.byandrev.habito.ui.screens.LoginScreen
