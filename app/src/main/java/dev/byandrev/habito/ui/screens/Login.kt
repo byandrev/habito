02424f517a7d3d62b363ec0cbc7e9b85786f8c53
Andres Parra <byandrev@gmail.com>
2025-10-14 22:04:58 -0500

    feat: add password visibility toggle to login screen
    This commit introduces a password visibility toggle on the login screen.
    
    It adds:
    - An icon button to the password field that allows users to show or hide the password they are typing.
    - A `PasswordVisualTransformation` to obscure the text when the password is not visible.
    - A keyboard option to ensure the keyboard is optimized for password entry.

--- app/src/main/java/dev/byandrev/habito/ui/screens/Login.kt
+++ app/src/main/java/dev/byandrev/habito/ui/screens/Login.kt
@@ -6,25 +6,49 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.byandrev.habito.ui.components.CustomButton
 import dev.byandrev.habito.ui.components.Logo
+import dev.byandrev.habito.utils.isValidPassword
 import dev.byandrev.habito.viewmodel.AuthViewModel
 
 @Composable
 fun LoginScreen(authViewModel: AuthViewModel = viewModel()) {
     val inputName = remember { mutableStateOf("") }
     val inputPassword = remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    val visualTransformation = if (passwordVisible) {
+        VisualTransformation.None
+    } else {
+        PasswordVisualTransformation()
+    }
+
+    val icon = if (passwordVisible)
+        Icons.Outlined.Visibility
+    else
+        Icons.Filled.VisibilityOff
 
     Surface(
         modifier = Modifier.fillMaxWidth().fillMaxHeight()
@@ -56,7 +80,22 @@ fun LoginScreen(authViewModel: AuthViewModel = viewModel()) {
                 label = {
                     Text("Password")
                 },
-                onValueChange = { value -> inputPassword.value = value }
+                onValueChange = { value ->
+                    if (isValidPassword(value) || (inputPassword.value.isNotEmpty() && value === "")) {
+                        inputPassword.value = value
+                    }
+                },
+                visualTransformation = visualTransformation,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                trailingIcon = {
+                    val description = if (passwordVisible) "Hide password" else "Show password"
+
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(imageVector = icon, contentDescription = description)
+                    }
+                },
             )
 
             CustomButton(
