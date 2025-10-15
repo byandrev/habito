9d18507480a37439313cde4064d6e84d0280cb5f
Andres Parra <byandrev@gmail.com>
2025-10-14 22:05:23 -0500

    refactor: fetch habits from ViewModel in HabitsList

--- app/src/main/java/dev/byandrev/habito/ui/components/HabitsList.kt
+++ app/src/main/java/dev/byandrev/habito/ui/components/HabitsList.kt
@@ -7,29 +7,26 @@ import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import dev.byandrev.habito.data.Habit
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.byandrev.habito.data.HabitCompletion
 import dev.byandrev.habito.data.getDailyHabits
-import java.time.DayOfWeek
+import dev.byandrev.habito.ui.AppViewModelProvider
+import dev.byandrev.habito.viewmodel.HabitViewModel
 import java.time.LocalDate
 
 @Composable
 fun HabitsList(
-    today: LocalDate
+    today: LocalDate,
+    viewModel: HabitViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
-    val habits = remember {
-        listOf(
-            Habit(id = 1, name = "Beber 2 litros de agua", schedule = setOf(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY)),
-            Habit(id = 2, name = "Rutina de ejercicios 30m", schedule = setOf(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY, DayOfWeek.FRIDAY)),
-            Habit(id = 3, name = "Leer 10 páginas", schedule = setOf(DayOfWeek.SUNDAY, DayOfWeek.TUESDAY, DayOfWeek.THURSDAY))
-        )
-    }
+    val habits by viewModel.habits.collectAsState()
 
     var completions by remember { mutableStateOf(listOf(
         HabitCompletion(habitId = 1, date = LocalDate.of(2025, 10, 6))
