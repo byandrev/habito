139c382994c894768f53d044c5eb545d79e84a34
Andres Parra <byandrev@gmail.com>
2025-10-14 22:04:48 -0500

    refactor: remove unused filter in HabitViewModel

--- app/src/main/java/dev/byandrev/habito/viewmodel/HabitViewModel.kt
+++ app/src/main/java/dev/byandrev/habito/viewmodel/HabitViewModel.kt
@@ -5,15 +5,12 @@ import androidx.lifecycle.viewModelScope
 import dev.byandrev.habito.data.Habit
 import dev.byandrev.habito.data.repositories.HabitsRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class HabitViewModel(private val habitsRepository: HabitsRepository) : ViewModel() {
-    private val _filter = MutableStateFlow<Boolean?>(null)
-
     @OptIn(ExperimentalCoroutinesApi::class)
     var habits: StateFlow<List<Habit>> = habitsRepository
         .getAllHabitsStream()
