df5b97f637b6501a1c7637a736a78f93375e322e
Andres Parra <byandrev@gmail.com>
2025-10-14 22:05:12 -0500

    feat: add reminder time picker to new habit screen

--- app/src/main/java/dev/byandrev/habito/ui/screens/NewHabit.kt
+++ app/src/main/java/dev/byandrev/habito/ui/screens/NewHabit.kt
@@ -1,16 +1,23 @@
 package dev.byandrev.habito.ui.screens
 
-import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Checkbox
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -20,19 +27,36 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.byandrev.habito.data.Habit
+import dev.byandrev.habito.ui.AppViewModelProvider
 import dev.byandrev.habito.ui.components.CustomButton
 import dev.byandrev.habito.ui.theme.HabitoTheme
+import dev.byandrev.habito.viewmodel.HabitViewModel
+import java.util.Calendar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewHabitScreen() {
+fun NewHabitScreen(
+    viewModel: HabitViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
     val scope = rememberCoroutineScope()
 
+    var isDialogTimeOpen by remember { mutableStateOf(false) }
     var name by remember { mutableStateOf("") }
     val frequency = remember { mutableStateListOf(false, false, false, false, false, false, false) }
     val namesDaysOfWeek = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+    val currentTime = Calendar.getInstance()
+
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = false,
+    )
 
     Surface(
         modifier = Modifier.padding(20.dp)
@@ -46,37 +70,110 @@ fun NewHabitScreen() {
             )
 
             Text(
-                modifier = Modifier.padding(top = HabitoTheme.dimens.paddingMedium),
+                modifier = Modifier.padding(vertical = HabitoTheme.dimens.paddingMedium),
                 text = "Frequency",
                 fontWeight = FontWeight.Bold
             )
 
             Column (
-                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(HabitoTheme.dimens.paddingNormal),
+                modifier = Modifier.padding(bottom = HabitoTheme.dimens.paddingLarge)
             ) {
-                frequency.forEachIndexed { index, isChecked ->
-                   Row (
-                       verticalAlignment = Alignment.CenterVertically,
-                   ){
-                       Checkbox(
-                           checked = isChecked,
-                           onCheckedChange = {
-                               frequency[index] = it
+                CustomButton(
+                    onTap = {},
+                    text = "Daily",
+                    textColor = MaterialTheme.colorScheme.surface,
+                    buttonColor = MaterialTheme.colorScheme.primary,
+                )
 
-                               Log.d("Checkbox", "Day ${index + 1} checked: $it")
-                           }
-                       )
-                       Text(text = namesDaysOfWeek[index])
-                   }
-               }
+                CustomButton(
+                    onTap = {},
+                    text = "Specific days",
+                    textColor = MaterialTheme.colorScheme.primary,
+                    buttonColor = MaterialTheme.colorScheme.surfaceDim,
+                )
             }
 
-            CustomButton(
-                onTap = {},
-                text = "Save",
-                textColor = MaterialTheme.colorScheme.surface,
-                buttonColor = MaterialTheme.colorScheme.primary,
+            Text(
+                modifier = Modifier.padding(bottom = HabitoTheme.dimens.paddingMedium),
+                text = "Reminder",
+                fontWeight = FontWeight.Bold
             )
+
+            Column (
+                modifier = Modifier.clickable {
+                    isDialogTimeOpen = true
+                }
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(HabitoTheme.dimens.buttonHeightNormal)
+                        .clip(RoundedCornerShape(HabitoTheme.dimens.roundedShapeNormal))
+                        .background(MaterialTheme.colorScheme.surfaceDim)
+                        .padding(HabitoTheme.dimens.paddingMedium),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Time",
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        text = "" + timePickerState.hour + ":" + timePickerState.minute,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+
+            if (isDialogTimeOpen) {
+                AlertDialog(
+                    text = {
+                        TimePicker(
+                            state = timePickerState,
+
+                            )
+                    },
+                    onDismissRequest = {
+                    },
+                    confirmButton = {
+                        TextButton(
+                            onClick = {
+                                isDialogTimeOpen = false
+                            }
+                        ) {
+                            Text("Confirm")
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(
+                            onClick = {
+                                isDialogTimeOpen = false
+                            }
+                        ) {
+                            Text("Dismiss")
+                        }
+                    }
+                )
+            }
+
+            Column (
+                modifier = Modifier.padding(top = HabitoTheme.dimens.paddingNormal)
+            ) {
+                CustomButton(
+                    onTap = {
+                        viewModel.addHabit(Habit(
+                            name = name
+                        ))
+                    },
+                    text = "Save",
+                    textColor = MaterialTheme.colorScheme.surface,
+                    buttonColor = MaterialTheme.colorScheme.primary,
+                )
+            }
+
         }
     }
 }
